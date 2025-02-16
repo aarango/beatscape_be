593b524ce15ae1e83a3f390f532b794e51fff334
Andres Bermudez <a.arango09@gmail.com>
2025-02-15 21:51:11 -0500

    update fns

--- functions/src/presentation/controllers/CreateUserController.js
+++ functions/src/presentation/controllers/CreateUserController.js
@@ -2,7 +2,7 @@
 
 const { onRequest } = require("firebase-functions/v2/https");
 const { normalizeString } = require("@utils/normaliceString");
-const { db } = require("@infrastructure/data/firebase/FirebaseConfig");
+const { db, auth } = require("@infrastructure/data/firebase/FirebaseConfig");
 const cors = require("cors")({ origin: true });
 
 exports.createUser = onRequest((req, res) => {
@@ -12,82 +12,80 @@ exports.createUser = onRequest((req, res) => {
     }
 
     const authHeader = req.headers.authorization;
-
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ error: "Autenticación requerida." });
     }
 
     try {
+      const idToken = authHeader.split("Bearer ")[1];
+      console.log("🚀 ~ cors ~ idToken:", idToken);
+
       const {
         email,
         password,
         displayName,
         canUseMultimedia,
-        cliendId,
-        cliendInfo,
+        clientId,
+        clientInfo,
         username,
       } = req.body;
-      if (!id) {
-        return res.status(400).json({ error: "El campo 'id' es obligatorio." });
-      }
 
       if (
         !email ||
         !password ||
         !displayName ||
-        !canUseMultimedia ||
-        !cliendId ||
-        !cliendInfo
+        canUseMultimedia === undefined ||
+        !clientId ||
+        !clientInfo
       ) {
-        return res.status(400).send({
+        return res.status(400).json({
           error: "invalid-argument",
-          message: "Email, password, and mainPath are required.",
-          success: false,
+          message:
+            "Email, password, displayName, clientId, and clientInfo are required.",
         });
       }
 
       if (!email.endsWith("@beatscape.com")) {
-        return res.status(400).send({
+        return res.status(400).json({
           error: "invalid-argument",
           message: "Email must end with @beatscape.com.",
-          success: false,
         });
       }
 
       const userRecord = await auth.createUser({
         email,
         password,
-        displayName: displayName || "",
+        displayName,
       });
 
       const customClaims = {
-        cliendId: cliendId,
-        cliendInfo: cliendInfo,
+        clientId,
+        clientInfo,
       };
 
       await auth.setCustomUserClaims(userRecord.uid, customClaims);
 
       await db
-        .collection(`enterprises/${cliendId}/headquarters`)
-        .doc(userRecord.uid)
+        .collection(`enterprises/${clientId}/headquarters`)
+        .doc(username)
         .set({
           email: userRecord.email,
           uid: userRecord.uid,
           username: normalizeString(username),
           displayName: userRecord.displayName,
-          canUseMultimedia: canUseMultimedia,
+          canUseMultimedia,
           createdAt: new Date(),
           status: true,
         });
 
       return res.status(200).json({
-        message: "Usuario  creado.",
-        data: updatedSong.data(),
-        ok: true,
+        message: "Usuario creado exitosamente.",
       });
     } catch (error) {
-      console.error("Error al editar creat:", error);
-      return res.status(500).json({ error: "Ocurrió un error al creat." });
+      console.error("Error al crear usuario:", error);
+      return res
+        .status(500)
+        .json({ error: "Ocurrió un error al crear usuario." });
     }
   });
 });
